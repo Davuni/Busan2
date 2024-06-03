0d1a0cb0e7f57c6c2584a822f7bc6552ea09093f
다빈 신 <dabin7223@gmail.com>
2024-06-03 23:50:32 +0900

    최종 수정

--- Busan2.c
+++ Busan2.c
@@ -48,8 +48,6 @@ int main(void) {
 
 	intro();
 
-	//열차의 길이와 확률p(%)를 입력 받는다.
-	//잘못된 입력을 받으면 프로그램 종료
 	int len, p;
 	printf("train length(%d~%d)>> ", LEN_MIN, LEN_MAX);
 	scanf_s("%d", &len);
@@ -62,9 +60,6 @@ int main(void) {
 		return 1;
 	}
 
-	//열차의 초기 상태를 출력한다. (0~lne-1)
-	//오른쪽부터 마동석(M)len-2, 좀비(Z)len-3, 빈 칸('')2개, 시민(C)len-6
-	//배열 사용x
 	int M, Z, C;
 	M = len - 2;
 	Z = len - 3;
@@ -99,25 +94,15 @@ int main(void) {
 	int turn = 1;
 	while (1) {
 
-		//프로그램 구성
-		//4초마다 시민 이동, 좀비 이동, 열차 출력, 시민, 좀비 상태 출력
-		//Windows.h:Sleep(millis);
-		//<Windows.h>
+		// 1-4. 시민 이동
 
-		//1-4. 시민 이동
-		//(100-p)% 확률로 왼쪽으로 1칸 이동하거나, p% 확률로 제자리에 대기
-		//p가 낮을수록 성공 가능성 증가, p가 높을수록 실패 가능성 증가(뒷장 참고)
 		int cr = rand() % 100 + 1;
 		if (cr <= 100 - p) {
 			C--;
 		}
 
-		//1-5. 좀비 이동
-		//좀비는 2턴 주기로 움직임
-		//1턴은 p% 확률로 왼쪽으로 1칸 이동하거나 (100-p)% 확률로 제자리에 대기
-		//2턴은 아무것도 하지 않음
-		//3턴은 p% 확률로 왼쪽으로 1칸 이동하거나 (100-p)% 확률로 제자리에 대기
-		//4턴은 아무것도 하지 않음 ...
+		// 1-5. 좀비 이동
+
 		int zr;
 		if (turn % 2 == 0) {
 			zr = 100 - p;
@@ -130,9 +115,8 @@ int main(void) {
 		}
 
 
-		//1-2, 1-3. 출력
-		//매 턴마다 
-		//1-2. 열차 상태 출력
+
+		// 1-2. 열차 상태 출력
 		for (int i = 0; i <= len - 1; i++) {
 			printf("#");
 		}
@@ -160,8 +144,8 @@ int main(void) {
 		}
 		printf("\n\n");
 
-		//1-3. 시민, 좀비 상태 출력
-		//시민 상태 출력
+		// 1-3. 시민, 좀비 상태 출력
+		// 시민 상태
 		if (cr <= 100 - p) {
 			printf("citizen : %d -> %d\n", C + 1, C);
 		}
@@ -169,12 +153,12 @@ int main(void) {
 			printf("citizen : stay %d\n", C);
 		}
 
-		//좀비 짝수턴 상태 출력
+		// 좀비 짝수턴
 		if (turn % 2 == 0) {
 			printf("zombie : stay %d (cannot move)\n\n\n", Z);
 		}
 		else {
-			//좀비 홀수턴 상태 출력
+			// 좀비 홀수턴 상태 출력
 			if (zr <= p) {
 				printf("zombie : %d -> %d\n\n\n", Z + 1, Z);
 			}
@@ -184,12 +168,11 @@ int main(void) {
 		}
 		turn++;
 
-		//시민이 왼쪽 끝(1번 열)에 도달하면 구출 성공
 		if (C == 1) {
 			printf("SUCCESS!");
 			break;
 		}
-		//좀비가 이동한 후, 시민 바로 옆 칸에 도착했으면 시민은 사망하고 구출 실패
+
 		if (Z == C + 1) {
 			printf("GAME OVER!");
 			break;
